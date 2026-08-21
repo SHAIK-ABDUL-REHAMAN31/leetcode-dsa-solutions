c9dd863cc17170d82cdf5cb06be9ebdfd39fae43
SHAIK-ABDUL-REHAMAN31 <mrabbu985@gmail.com>
2026-08-21 12:22:35 +0530

    Time: 0 ms (100%), Space: 54.8 MB (57.06%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+   let map = new Map();
+   for(let i =  0;  i < nums.length ; i++){
+    let required = target - nums[i];
+
+    if (map.has(required)){
+        return [map.get(required) , i];
+    }
+    map.set(nums[i] , i)
+  }    
+};
