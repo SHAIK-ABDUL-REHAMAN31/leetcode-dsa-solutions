ba1d6171715b93792b62632a7ae81b8998669b7f
SHAIK-ABDUL-REHAMAN31 <mrabbu985@gmail.com>
2026-08-21 16:25:42 +0530

    Time: 1 ms (73.71%), Space: 54.9 MB (84.35%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,26 +3,11 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
+  let result = 0;
 
-    let frequency = new Map ();
-    let output ; 
+  for (const num of nums) {
+    result ^= num;
+  }
 
-    for (const num of nums){
-
-        frequency.set(num , (frequency.get(num)|| 0) +1 );
-
-        
-    }
-
-
-    for (const [num , count ] of frequency){
-        if (count === 1){
-            return num;
-        }
-    }
-
-   
-
-    
-    
+  return result;
 };
