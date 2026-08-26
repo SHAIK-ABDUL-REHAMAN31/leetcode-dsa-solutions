a66be5b8a959cbb3438fe1cfb7642a6f6878a49a
SHAIK-ABDUL-REHAMAN31 <mrabbu985@gmail.com>
2026-08-26 23:56:19 +0530

    Time: 40 ms (48.72%), Space: 81.1 MB (28.58%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -2,31 +2,26 @@
  * @param {number[]} nums
  * @return {number}
  */
-var longestConsecutive = function (nums) {
-    if (nums.length === 0) {
-        return 0;
-    }
+var longestConsecutive = function(nums) {
 
-    nums.sort((a, b) => a - b);
+    let set = new Set(nums);
+    let longest = 0;
 
-    let longest = 1;
-    let currentStreak = 1;
+    for (let num of set) {
 
-    for (let i = 1; i < nums.length; i++) {
-        // Ignore duplicate numbers
-        if (nums[i] === nums[i - 1]) {
-            continue;
+      
+        if (!set.has(num - 1)) {
+
+            let current = num;
+            let streak = 1;
+
+            while (set.has(current + 1)) {
+                current++;
+                streak++;
+            }
+
+            longest = Math.max(longest, streak);
         }
-
-        // Consecutive: for example, 2 follows 1
-        if (nums[i] === nums[i - 1] + 1) {
-            currentStreak++;
-        } else {
-            // Sequence broke; begin a new sequence
-            currentStreak = 1;
-        }
-
-        longest = Math.max(longest, currentStreak);
     }
 
     return longest;
