e585f94a0207c9a0c9fbdc2f7da728cf76f25722
SHAIK-ABDUL-REHAMAN31 <mrabbu985@gmail.com>
2026-09-10 00:18:50 +0530

    Time: 38 ms (68.91%), Space: 59.9 MB (65.58%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+
+    let map = new Map();
+
+    let left = 0;
+    let longest = 0;
+    
+    for(let right = 0 ; right < s.length ; right++){
+
+        if(map.has(s[right])){
+
+            left = Math.max(left , map.get(s[right]) +1 );
+
+        }
+
+        map.set(s[right] , right );
+
+        longest = Math.max(longest , right - left + 1 );
+
+       
+    }
+
+    return longest;
+    
+};
