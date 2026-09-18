0306aba3228d94bd764fae5d453b98a524b69590
SHAIK-ABDUL-REHAMAN31 <mrabbu985@gmail.com>
2026-09-18 22:29:50 +0530

    Time: 10 ms (19.97%), Space: 60.7 MB (66.76%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return (
+                isPalindrome(left + 1, right) ||
+                isPalindrome(left, right - 1)
+            );
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+
+    function isPalindrome(left, right) {
+        while (left < right) {
+            if (s[left] !== s[right]) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
