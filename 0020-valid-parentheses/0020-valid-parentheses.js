5ff3cbf13a8e33095efa33e963987b9697785ef7
SHAIK-ABDUL-REHAMAN31 <mrabbu985@gmail.com>
2026-09-21 23:28:31 +0530

    Time: 0 ms (100%), Space: 57.5 MB (17.13%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+
+    const stack  = [];
+    const pairs = {
+        ')' : '(',
+        '}' : '{',
+        ']' : '['
+    };
+
+    for(const ch of s){
+        
+        if(ch === '(' || ch === '[' || ch === '{'){
+            stack.push(ch);
+        }else{
+            if(stack.length === 0){
+                return false;
+
+            }
+
+            const top = stack.pop();
+
+            if(top !== pairs[ch] ){
+                return false;
+            }
+
+
+        }
+    }
+
+    return stack.length === 0;
+    
+};
